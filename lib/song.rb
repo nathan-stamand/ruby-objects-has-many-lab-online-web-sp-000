0cd8fa4a1a6ec6bc663ad10af99857dfe244cae0
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-27 16:57:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,22 @@
-class Post
-  attr_accessor :artist, :title
+class Song
+  attr_accessor :artist, :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def artist_name 
+    if !artist
+      nil 
+    else 
+      artist 
+    end
+  end
   
 end
