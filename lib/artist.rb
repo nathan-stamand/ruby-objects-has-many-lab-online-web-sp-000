e7d31b671a1a1de6b2aa1f04ca7c7885369f5413
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-27 17:00:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,21 @@
 class Artist 
   attr_accessor :song, :name
   
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def add_song(song)
+    song.artist = self 
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self 
+  end 
+  
+  def self.song_count
+    Song.all 
+  end
+  
 end 
