fbb7840bc0897841c9a1fc6d54328031591e4e84
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-27 16:15:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,17 @@
 class Author
   attr_accessor :post, :name
   
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end
+  
+  def posts 
+    @posts
+  end
+  
+  def add_post(new_post)
+    new_post = Post.new
+  end 
+  
 end 
